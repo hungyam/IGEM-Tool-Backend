85d8956eeac7be648a8bf64908d373d51281d0cd
hungyam <hungyam@yeah.net>
2022-09-05 15:35:00 +0800

    Server v2

--- Resistant_System/models.py
+++ Resistant_System/models.py
@@ -10,7 +10,7 @@ class Species(models.Model):
 
 
 class System(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
     species = models.ForeignKey('Species', on_delete=models.CASCADE)
 
     def __str__(self):
