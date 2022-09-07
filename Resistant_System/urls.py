baba0a000cfa92b852252fe44e01c0ebd5592eb7
hungyam <hungyam@yeah.net>
2022-09-07 17:26:07 +0800

    Server v2 testing fix

--- Resistant_System/urls.py
+++ Resistant_System/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('remove_yes_i_need/', views.reloadData, name='reload'),
 
     path('data/<int:page>/', views.data, name='data'),
+    path('detail_data/<int:id>/', views.detail_data),
     path('species/', views.species, name='special'),
     path('system/', views.system, name='system'),
     path('mes/', views.mes, name='message'),
@@ -14,5 +15,5 @@ urlpatterns = [
     path('download/', views.download, name='download'),
     path('upload_csv/<int:type>/', views.upload_csv, name='upload_csv'),
 
-    path('test/', views.join_table)
+    # path('test/', views.join_table)
 ]
